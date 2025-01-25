dd93d30c98269a976d428e143f8662666fbfcef0
Muhammed Gamal <56918654+muhammedgamal30@users.noreply.github.com>
2025-01-25 18:12:48 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,70 @@
+using CourseManagementAPI.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+      options.RequireHttpsMetadata = false;
+      options.SaveToken = true;
+      options.TokenValidationParameters = new TokenValidationParameters
+      {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("YourSuperSecretKey")),
+        ValidateIssuer = false,
+        ValidateAudience = false
+      };
+    });
+
+builder.Services.AddAuthorization();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast =  Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi();
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
---
+++ TrainerServiceTests.cs
@@ -0,0 +1,28 @@
+﻿using CourseManagementAPI.Contollers;
+using CourseManagementAPI.Data;
+using CourseManagementAPI.Models;
+using Moq;
+using Xunit;
+
+namespace CourseManagementAPI
+{
+  public class TrainerServiceTests
+  {
+    private readonly Mock<ApplicationDbContext> _mockContext;
+    private readonly TrainersController _controller;
+
+    public TrainerServiceTests()
+    {
+      _mockContext = new Mock<ApplicationDbContext>();
+      _controller = new TrainersController(_mockContext.Object);
+    }
+
+    [Fact]
+    public async Task GetTrainers_ReturnsListOfTrainers()
+    {
+      var result = await _controller.GetTrainers();
+      Assert.IsType<List<Trainer>>(result.Value);
+    }
+  }
+
+}
